69901c3afbae9d465fe7b2be278454c6b4afaf80
z-a-y <isaiah@stanford.edu>
2022-06-23 18:12:59 -0700

    Updates

---
+++ precache-manifest.f57fc88a394d746c03657a87a823cf62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "518f0004a0e0db39389a",
+    "url": "/DigTheSounds/static/css/main.43c4a3b1.chunk.css"
+  },
+  {
+    "revision": "518f0004a0e0db39389a",
+    "url": "/DigTheSounds/static/js/main.518f0004.chunk.js"
+  },
+  {
+    "revision": "1f8777550d0c14195a51",
+    "url": "/DigTheSounds/static/js/1.1f877755.chunk.js"
+  },
+  {
+    "revision": "60423c926b2a4a1b324f",
+    "url": "/DigTheSounds/static/js/runtime~main.60423c92.js"
+  },
+  {
+    "revision": "a3adbc7764a0d9bbe3d562f9e6b8db34",
+    "url": "/DigTheSounds/index.html"
+  }
+];
