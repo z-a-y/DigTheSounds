79952f321539f54e6ffc84921a4f3f9e621fbb74
z-a-y <30961774+z-a-y@users.noreply.github.com>
2022-06-23 19:32:01 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "612ced444c3b4b5bb5359d317aaf8dba";
-export const redirectUri = "https://z-a-y.github.io/DigTheSounds";
+export const redirectUri = "https://z-a-y.github.io/DigTheSounds/";
 export const scopes = [
     "user-top-read",
 ];
