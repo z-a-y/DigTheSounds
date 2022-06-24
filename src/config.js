bf2ebcd7e056fce1843c5848a5582461d20103ad
z-a-y <isaiah@stanford.edu>
2022-06-23 18:43:07 -0700

    Add config

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "612ced444c3b4b5bb5359d317aaf8dba";
+export const redirectUri = "https://z-a-y.github.io/DigTheSounds/redirect";
+export const scopes = [
+    "user-top-read",
+];
